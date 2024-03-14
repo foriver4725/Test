16f0445e7a3ea396c66a0bc810e8c661b1f1e5ea
foriver4725 <tk230001@tks.iput.ac.jp>
2024-03-14 16:57:28 +0900

    tmp

---
+++ Assets/Scripts/EventTrigger/EventTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventTrigger
+{
+    public class EventTrigger : MonoBehaviour
+    {
+        public void DebugLog()
+        {
+            Debug.Log(1);
+        }
+    }
+}
