79fa41111ac7cb3f5b287a04d223f1d12fcbf280
foriver4725 <hrt67@icloud.com>
2024-08-03 23:50:13 +0900

    tmp

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ex;
+
+namespace CustomMethods
+{
+    public class Test : MonoBehaviour
+    {
+        #region
+        private void Awake()
+        {
+            enabled = true;
+        }
+        #endregion
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
