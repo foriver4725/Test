2f27e2d433c3eea17e5421250280d47b97940556
foriver4725 <hrt67@icloud.com>
2024-08-05 17:15:33 +0900

    tmp

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Ex;
-
 namespace CustomMethods
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Ex;
+
     public class Test : MonoBehaviour
     {
         #region
